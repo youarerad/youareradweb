609ec1ca4035c90fbf4680bb2fe5d0cdf0bdac11
Guillermo Cava <gcavanunez@gmail.com>
2022-06-10 00:00:48 -0500

    feat: Alert User is already subscribed - on `create-contact` return valid message - use `contactRouter` to access message on` FooterNewsletter`

--- src/components/Footer/FooterNewsletter.tsx
+++ src/components/Footer/FooterNewsletter.tsx
@@ -55,7 +55,7 @@ export default function FooterNewsletter() {
         disabled={submit}
         className="w-full px-3 py-1 text-base font-bold transition-all duration-300 ease-linear bg-white border-2 border-black elative rounded-xl hover:shadow-none focus-within:shadow-none hover:bg-black hover:text-white shadow-deep focus:bg-green-light disabled:bg-green-light disabled:shadow-none disabled:pointer-events-none focus-within:text-black outline-none focus:ring-4 focus:ring-secondary-light"
       >
-        {isSuccess ? 'Subscribed!' : 'Subscribe'}
+        {isSuccess ? contactRouter.data?.message : 'Subscribe'}
         {isError ?? contactRouter.error?.message}
       </button>
     </Form>
