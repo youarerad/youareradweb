8a5bf67e53e3fd0c4ee3934c297a5b73d74aadc3
Jason Docton <Jdocton@gmail.com>
2022-06-15 13:34:19 -0700

    Production urls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      ? `https://https://youarerad.org/api/trpc`
       : 'http://localhost:3000/api/trpc'
 
     const ONE_DAY_SECONDS = 60 * 60 * 24
