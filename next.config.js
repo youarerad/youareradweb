a117db2161f27231f95a159cb5ec7150d490c943
Jason Docton <Jdocton@gmail.com>
2022-06-15 15:10:22 -0700

    removed preact

--- next.config.js
+++ next.config.js
@@ -13,22 +13,7 @@ const nextConfig = {
     locales: ['en-US'],
     defaultLocale: 'en-US',
   },
-  experimental: { optimizeCss: true },
-
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
+  experimental: { optimizeCss: true, browsersListForSwc: true },
 }
 
 module.exports = nextConfig
