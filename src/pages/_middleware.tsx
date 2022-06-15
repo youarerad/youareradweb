9b65f8692de732c2d981737d74ab21fd69511c45
Jason Docton <Jdocton@gmail.com>
2022-06-15 13:50:20 -0700

    content security policy

---
+++ src/pages/_middleware.tsx
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+
+export function middleware() {
+  const ContentSecurityPolicy = `
+  frame-src 'self' https://checkout.stripe.com https://js.stripe.com https://hooks.stripe.com;
+  frame-ancestors 'none';
+  object-src 'self';
+  base-uri 'self' ;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://checkout.stripe.com https://js.stripe.com https://cdn.usefathom.com/script.js;
+  child-src 'self';
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src 'self' *.cloudinary.com ;
+  connect-src 'self' https://api.stripe.com https://checkout.stripe.com https://vitals.vercel-insights.com/v1/vitals;
+  upgrade-insecure-requests;
+  block-all-mixed-content;
+  font-src 'self';
+  form-action 'self';
+  default-src 'self';
+  `
+
+  const response = NextResponse.next()
+
+  response.headers.set('Content-Security-Policy', ContentSecurityPolicy.replace(/\n/g, ''))
+  response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin')
+  response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()')
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
+  response.headers.set('X-Frame-Options', 'DENY')
+  response.headers.set('X-Content-Type-Options', 'nosniff')
+  response.headers.set('X-DNS-Prefetch-Control', 'on')
+  response.headers.set('X-XSS-Protection', '1; mode=block')
+
+  return response
+}
