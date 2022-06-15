bddc83b9651e0381776bb840eb91e070d0eb0ffe
Jason Docton <Jdocton@gmail.com>
2022-06-15 15:04:56 -0700

    preact for production

--- next.config.js
+++ next.config.js
@@ -13,7 +13,22 @@ const nextConfig = {
     locales: ['en-US'],
     defaultLocale: 'en-US',
   },
-  experimental: { optimizeCss: true },
+  experimental: { optimizeCss: true, browsersListForSwc: true },
+
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
