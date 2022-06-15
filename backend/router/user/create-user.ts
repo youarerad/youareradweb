a0688ce739c4a8dff3caaf9372a64948a902d538
Jason Docton <Jdocton@gmail.com>
2022-06-15 14:25:37 -0700

    try/catch

--- backend/router/user/create-user.ts
+++ backend/router/user/create-user.ts
@@ -6,28 +6,32 @@ export async function createUserOneTimeDonation(
   amount: number,
   customer_id: string
 ) {
-  await prisma.donation.create({
-    data: {
-      name: name,
-      email: email,
-      amount: amount / 100,
-      payment_status: 'SUCCESS',
-      payment_method: 'STRIPE',
-      payment_type: 'ONETIME',
-      customer_id: customer_id,
-      User: {
-        connectOrCreate: {
-          where: {
-            email: email,
-          },
-          create: {
-            name: name,
-            email: email,
+  try {
+    await prisma.donation.create({
+      data: {
+        name: name,
+        email: email,
+        amount: amount / 100,
+        payment_status: 'SUCCESS',
+        payment_method: 'STRIPE',
+        payment_type: 'ONETIME',
+        customer_id: customer_id,
+        User: {
+          connectOrCreate: {
+            where: {
+              email: email,
+            },
+            create: {
+              name: name,
+              email: email,
+            },
           },
         },
       },
-    },
-  })
+    })
+  } catch {
+    throw new Error('Error creating user one time donation')
+  }
 }
 
 export async function createUserMonthlyDonation(
@@ -36,27 +40,31 @@ export async function createUserMonthlyDonation(
   amount: number,
   customer_id: string
 ) {
-  await prisma.donation.create({
-    data: {
-      name: name,
-      email: email,
-      amount: amount / 100,
-      payment_status: 'SUCCESS',
-      payment_method: 'STRIPE',
-      payment_type: 'MONTHLY',
-      customer_id: customer_id,
-      User: {
-        connectOrCreate: {
-          where: {
-            email: email,
-          },
-          create: {
-            name: name,
-            email: email,
-            is_monthly: true,
+  try {
+    await prisma.donation.create({
+      data: {
+        name: name,
+        email: email,
+        amount: amount / 100,
+        payment_status: 'SUCCESS',
+        payment_method: 'STRIPE',
+        payment_type: 'MONTHLY',
+        customer_id: customer_id,
+        User: {
+          connectOrCreate: {
+            where: {
+              email: email,
+            },
+            create: {
+              name: name,
+              email: email,
+              is_monthly: true,
+            },
           },
         },
       },
-    },
-  })
+    })
+  } catch {
+    throw new Error('Error creating user monthly donation')
+  }
 }
