c962a57fdf08f9437ffbab8ca7e5219cdaa2182b
Jason Docton <Jdocton@gmail.com>
2022-06-29 15:03:15 -0700

    Twitter data fixed

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,170 +5,195 @@ import { useField } from '@unform/core'
 /* Input is a polymorphic component designed to work directly with our unform forms. We've explored several approaches to input and form components, with this approach both being the result of that exploration and simply not wanting to spend more time optimizing. With this current approach, the Input and its state are all managed by unform. Styling here may be better with a module.css approach, especially as much of the styling is the same.  */
 
 interface Props {
-  name: string
-  label?: string
-  variant: 'text' | 'textarea' | 'radio' | 'email' | 'number'
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
-  radioGroup?: string
-  checked?: boolean
+	name: string
+	label?: string
+	variant: 'text' | 'textarea' | 'radio' | 'email' | 'number' | 'phone' | 'zipcode'
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+	radioGroup?: string
+	checked?: boolean
 }
 type InputProps = InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> & Props
 
 export default function Input({
-  name,
-  label,
-  onChange,
-  radioGroup,
-  checked,
-  ...props
+	name,
+	label,
+	onChange,
+	radioGroup,
+	checked,
+	...props
 }: InputProps) {
-  const inputRef = useRef(null)
-  const { fieldName, defaultValue, registerField, error, clearError } = useField(name)
-  const { variant = 'text', ...rest } = props
+	const inputRef = useRef(null)
+	const { fieldName, defaultValue, registerField, error, clearError } = useField(name)
+	const { variant = 'text', ...rest } = props
 
-  useEffect(() => {
-    registerField({
-      name: fieldName,
-      ref: inputRef,
-      getValue: (ref) => {
-        return ref.current.value
-      },
-      setValue: (ref, value) => {
-        ref.current.value = value
-      },
-      clearValue: (ref) => {
-        ref.current.value = ''
-      },
-    })
-  }, [fieldName, registerField])
+	useEffect(() => {
+		registerField({
+			name: fieldName,
+			ref: inputRef,
+			getValue: (ref) => {
+				return ref.current.value
+			},
+			setValue: (ref, value) => {
+				ref.current.value = value
+			},
+			clearValue: (ref) => {
+				ref.current.value = ''
+			},
+		})
+	}, [fieldName, registerField])
 
-  if (variant === 'email') {
-    return (
-      <div>
-        {label && (
-          <label className="font-semibold text-gray" htmlFor={fieldName}>
-            {label}
-          </label>
-        )}
-        <div className="relative group">
-          <input
-            id={fieldName}
-            ref={inputRef}
-            type="email"
-            onFocus={clearError}
-            disabled={props.disabled}
-            defaultValue={defaultValue}
-            {...rest}
-            className={classNames(
-              error ? 'border-red-light' : 'border-gray-light',
-              'relative block w-full px-10 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
-            )}
-          />
-          <div className="inset-y-0 left-0 pl-3 flex items-center pointer-events-none absolute select-none">
-            <svg
-              aria-hidden
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7 text-gray-light"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-              />
-            </svg>
-          </div>
-        </div>
-        {error && <span className="text-red text-sm">{error}</span>}
-      </div>
-    )
-  }
+	if (variant === 'email') {
+		return (
+			<div>
+				{label && (
+					<label className="font-semibold text-gray" htmlFor={fieldName}>
+						{label}
+					</label>
+				)}
+				<div className="relative group">
+					<input
+						id={fieldName}
+						ref={inputRef}
+						type="email"
+						onFocus={clearError}
+						disabled={props.disabled}
+						defaultValue={defaultValue}
+						{...rest}
+						className={classNames(
+							error ? 'border-red-light' : 'border-gray-light',
+							'relative block w-full px-10 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
+						)}
+					/>
+					<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none select-none">
+						<svg
+							aria-hidden
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-7 w-7 text-gray-light"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+							/>
+						</svg>
+					</div>
+				</div>
+				{error && <span className="text-sm text-red">{error}</span>}
+			</div>
+		)
+	}
 
-  if (variant === 'radio') {
-    return (
-      <div className="relative w-full group rounded-xl">
-        <input
-          className="w-full sr-only peer"
-          type="radio"
-          name={radioGroup}
-          id={fieldName}
-          ref={inputRef}
-          {...rest}
-          checked={checked}
-          onChange={onChange}
-        />
-        <label
-          htmlFor={fieldName}
-          className="relative z-10 flex items-center justify-center w-full p-3 font-bold text-center transition-all duration-100 border-2 cursor-pointer border-gray-light peer-checked:bg-secondary peer-checked:border-secondary peer-checked:text-white peer-checked:shadow-xl peer-checked:drop-shadow-sm rounded-xl  hover:bg-secondary hover:border-secondary hover:text-white"
-        >
-          {name}
-        </label>
-      </div>
-    )
-  }
+	if (variant === 'radio') {
+		return (
+			<div className="relative w-full group rounded-xl">
+				<input
+					className="w-full sr-only peer"
+					type="radio"
+					name={radioGroup}
+					id={fieldName}
+					ref={inputRef}
+					{...rest}
+					checked={checked}
+					onChange={onChange}
+				/>
+				<label
+					htmlFor={fieldName}
+					className="relative z-10 flex items-center justify-center w-full p-3 font-bold text-center transition-all duration-100 border-2 cursor-pointer border-gray-light peer-checked:bg-secondary peer-checked:border-secondary peer-checked:text-white peer-checked:shadow-xl peer-checked:drop-shadow-sm rounded-xl hover:bg-secondary hover:border-secondary hover:text-white"
+				>
+					{name}
+				</label>
+			</div>
+		)
+	}
 
-  if (variant === 'number') {
-    return (
-      <input
-        className="relative z-10 flex items-center justify-center w-full p-3 font-bold transition-all duration-100 border-2 cursor-pointer border-gray-light rounded-xl hover:border-secondary outline-none"
-        type="number"
-        id={fieldName}
-        ref={inputRef}
-        defaultValue={defaultValue}
-        {...rest}
-        onChange={onChange}
-      />
-    )
-  }
+	if (variant === 'number') {
+		return (
+			<input
+				className="relative z-10 flex items-center justify-center w-full p-3 font-bold transition-all duration-100 border-2 outline-none cursor-pointer border-gray-light rounded-xl hover:border-secondary"
+				type="number"
+				id={fieldName}
+				ref={inputRef}
+				defaultValue={defaultValue}
+				{...rest}
+				onChange={onChange}
+			/>
+		)
+	}
 
-  if (variant === 'textarea') {
-    return (
-      <div className="relative group">
-        {label && (
-          <label className="font-semibold text-gray" htmlFor={fieldName}>
-            {label}
-          </label>
-        )}
-        <textarea
-          id={fieldName}
-          rows={4}
-          ref={inputRef}
-          onFocus={clearError}
-          defaultValue={defaultValue}
-          {...rest}
-          className={classNames(
-            error ? 'border-red-light' : 'border-gray-light',
-            'relative block w-full px-3 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
-          )}
-        />
-        {error && <span className="text-red text-sm">{error}</span>}
-      </div>
-    )
-  }
+	if (variant === 'textarea') {
+		return (
+			<div className="relative group">
+				{label && (
+					<label className="font-semibold text-gray" htmlFor={fieldName}>
+						{label}
+					</label>
+				)}
+				<textarea
+					id={fieldName}
+					rows={4}
+					ref={inputRef}
+					onFocus={clearError}
+					defaultValue={defaultValue}
+					{...rest}
+					className={classNames(
+						error ? 'border-red-light' : 'border-gray-light',
+						'relative block w-full px-3 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
+					)}
+				/>
+				{error && <span className="text-sm text-red">{error}</span>}
+			</div>
+		)
+	}
 
-  return (
-    <div className="relative group">
-      {label && (
-        <label className="font-semibold text-gray" htmlFor={fieldName}>
-          {label}
-        </label>
-      )}
-      <input
-        id={fieldName}
-        ref={inputRef}
-        onFocus={clearError}
-        defaultValue={defaultValue}
-        type="text"
-        {...rest}
-        className={classNames(
-          error ? 'border-red-light' : 'border-gray-light',
-          'relative block w-full px-3 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
-        )}
-      />
-      {error && <span className="text-red text-sm">{error}</span>}
-    </div>
-  )
+	if (variant === 'phone') {
+		return (
+			<div className="relative group">
+				{label && (
+					<label className="font-semibold text-gray" htmlFor={fieldName}>
+						{label}
+					</label>
+				)}
+				<input
+					id={fieldName}
+					ref={inputRef}
+					onFocus={clearError}
+					defaultValue={defaultValue}
+					type="tel"
+					{...rest}
+					className={classNames(
+						error ? 'border-red-light' : 'border-gray-light',
+						'relative block w-full px-3 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
+					)}
+				/>
+				{error && <span className="text-sm text-red">{error}</span>}
+			</div>
+		)
+	}
+
+	return (
+		<div className="relative group">
+			{label && (
+				<label className="font-semibold text-gray" htmlFor={fieldName}>
+					{label}
+				</label>
+			)}
+			<input
+				id={fieldName}
+				ref={inputRef}
+				onFocus={clearError}
+				defaultValue={defaultValue}
+				type="text"
+				{...rest}
+				className={classNames(
+					error ? 'border-red-light' : 'border-gray-light',
+					'relative block w-full px-3 py-2 text-black transition-all duration-300 ease-in-out border-2 rounded-xl bg-secondary-light bg-opacity-10 focus-within:bg-white focus-within:shadow-sm hover:bg-white disabled:bg-gray-light disabled:bg-opacity-50 disabled:placeholder-transparent outline-none focus:ring-4 focus:ring-secondary-light'
+				)}
+			/>
+			{error && <span className="text-sm text-red">{error}</span>}
+		</div>
+	)
 }
--- src/components/Tweet/TweetData.ts
+++ src/components/Tweet/TweetData.ts
@@ -1,108 +1,108 @@
 /* TweetData contains all of the tweets we will be displaying on the home page. The naming of each variable here must be exact to what the Tweet component expects. */
 
 export const twitterFundraiserData = [
-  {
-    tweetId: '1393354511662653440',
-    userProfileImage:
-      'https://res.cloudinary.com/df23ubjbb/image/upload/c_scale,w_266/v1639003083/NikkiTwitter.webp',
-    userName: 'Nikki',
-    isVerified: true,
-    userHandle: 'Nihaachu',
-    tweetBody:
-      'Thank you so much for this amazing stream! I cannot put into words how thankful I am for all your support and the love from my community. we also managed to completely smash the donation goal for @youarerad!!!',
-    tweetTime: '4:56pm',
-    tweetDate: 'May 14, 2021',
-    tweetComments: '955',
-    tweetRetweets: '2.6k',
-    tweetLikes: '98.7k',
-  },
-  {
-    tweetId: '1395528530990096387',
-    userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1639008470/Anita.webp',
-    userName: 'Sweet Anita',
-    isVerified: true,
-    userHandle: 'sweetanita',
-    tweetBody:
-      'Today was one of the best days of my life! We raised $57,000 For RAD today in less than 4 hours. I LOVE MY COMMUNITY SO MUCH 🥳',
-    tweetTime: '4:55pm',
-    tweetDate: 'May 20, 2021',
-    tweetComments: '33',
-    tweetRetweets: '6',
-    tweetLikes: '1.5k',
-  },
-  {
-    tweetId: '1393982815176331264',
-    userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1641851960/Miko.webp',
-    userName: 'Miko',
-    isVerified: true,
-    userHandle: 'thecodemiko',
-    tweetBody:
-      'Im doing my first Charity stream today! All Donation proceeds go to a mental health cause: Rise Above the Disorder!!',
-    tweetTime: '10:32am',
-    tweetDate: 'May 16, 2021',
-    tweetComments: '988',
-    tweetRetweets: '44',
-    tweetLikes: '12',
-  },
+	{
+		tweetId: '1393354511662653440',
+		userProfileImage:
+			'https://res.cloudinary.com/df23ubjbb/image/upload/c_scale,w_266/v1639003083/NikkiTwitter.webp',
+		userName: 'Nikki',
+		isVerified: true,
+		userHandle: 'Nihaachu',
+		tweetBody:
+			'Thank you so much for this amazing stream! I cannot put into words how thankful I am for all your support and the love from my community. we also managed to completely smash the donation goal for @youarerad!!!',
+		tweetTime: '4:56pm',
+		tweetDate: 'May 14, 2021',
+		tweetComments: '955',
+		tweetRetweets: '2.6k',
+		tweetLikes: '98.7k',
+	},
+	{
+		tweetId: '1395528530990096387',
+		userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1639008470/Anita.webp',
+		userName: 'Sweet Anita',
+		isVerified: true,
+		userHandle: 'sweetanita',
+		tweetBody:
+			'Today was one of the best days of my life! We raised $57,000 For RAD today in less than 4 hours. I LOVE MY COMMUNITY SO MUCH 🥳',
+		tweetTime: '4:55pm',
+		tweetDate: 'May 20, 2021',
+		tweetComments: '33',
+		tweetRetweets: '6',
+		tweetLikes: '1.5k',
+	},
+	{
+		tweetId: '1393982815176331264',
+		userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1641851960/Miko.webp',
+		userName: 'Miko',
+		isVerified: true,
+		userHandle: 'thecodemiko',
+		tweetBody:
+			'Im doing my first Charity stream today! All Donation proceeds go to a mental health cause: Rise Above the Disorder!!',
+		tweetTime: '10:32am',
+		tweetDate: 'May 16, 2021',
+		tweetComments: '12',
+		tweetRetweets: '44',
+		tweetLikes: '988',
+	},
 ]
 
 export const twitterTestimonialData = [
-  {
-    tweetId: '1473400672322129926',
-    userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1640749397/Masayoshi.jpg',
-    userName: 'Masayoshi',
-    isVerified: true,
-    userHandle: 'ItzMasayoshi',
-    tweetBody:
-      'shout out to @YouAreRAD for helping my family, friends, and myself for doing all the heavy lifting when finding us therapists. If you are also intimitweetDated to get help like I was, consider checking out RAD 😇',
-    tweetTime: '1:11 PM',
-    tweetDate: 'Dec 21, 2021',
-    tweetComments: '16',
-    tweetRetweets: '66',
-    tweetLikes: '3.9k',
-  },
-  {
-    tweetId: '1423331897158029312',
-    userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1638319109/John.jpg',
-    userName: 'John - BLM 🖤',
-    userHandle: 'J5isLive',
-    tweetBody:
-      "I'm thankful for @YouAreRAD, which has helped me return to going to therapy on a weekly basis and helps pay for a number of the expenses related to my mental health needs.",
-    tweetTime: '10:15am',
-    tweetDate: 'Aug 5, 2021',
-    tweetComments: '3',
-    tweetRetweets: '1',
-    tweetLikes: '5',
-  },
-  {
-    tweetId: '1379889888292573186',
-    userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1640755849/ETA.jpg',
-    userName: 'Evolved Talent Agency',
-    isVerified: true,
-    userHandle: 'EvolvedTalent',
-    tweetBody:
-      "We're excited to announce our partnership with @YouAreRAD! Mental health awareness has always been of utmost importance for us, and with RAD, we can't wait to continue this education for our clients, employees, and the entire virtual creator space.",
+	{
+		tweetId: '1473400672322129926',
+		userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1640749397/Masayoshi.jpg',
+		userName: 'Masayoshi',
+		isVerified: true,
+		userHandle: 'ItzMasayoshi',
+		tweetBody:
+			'shout out to @YouAreRAD for helping my family, friends, and myself for doing all the heavy lifting when finding us therapists. If you are also intimitweetDated to get help like I was, consider checking out RAD 😇',
+		tweetTime: '1:11 PM',
+		tweetDate: 'Dec 21, 2021',
+		tweetComments: '16',
+		tweetRetweets: '66',
+		tweetLikes: '3.9k',
+	},
+	{
+		tweetId: '1423331897158029312',
+		userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1638319109/John.jpg',
+		userName: 'John - BLM 🖤',
+		userHandle: 'J5isLive',
+		tweetBody:
+			"I'm thankful for @YouAreRAD, which has helped me return to going to therapy on a weekly basis and helps pay for a number of the expenses related to my mental health needs.",
+		tweetTime: '10:15am',
+		tweetDate: 'Aug 5, 2021',
+		tweetComments: '3',
+		tweetRetweets: '1',
+		tweetLikes: '5',
+	},
+	{
+		tweetId: '1379889888292573186',
+		userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1640755849/ETA.jpg',
+		userName: 'Evolved Talent Agency',
+		isVerified: true,
+		userHandle: 'EvolvedTalent',
+		tweetBody:
+			"We're excited to announce our partnership with @YouAreRAD! Mental health awareness has always been of utmost importance for us, and with RAD, we can't wait to continue this education for our clients, employees, and the entire virtual creator space.",
 
-    tweetTime: '1:12pm',
-    tweetDate: 'Apr 7, 2021',
-    tweetComments: '2',
-    tweetRetweets: '3',
-    tweetLikes: '25',
-  },
+		tweetTime: '1:12pm',
+		tweetDate: 'Apr 7, 2021',
+		tweetComments: '2',
+		tweetRetweets: '3',
+		tweetLikes: '25',
+	},
 ]
 
 export const therapyTweetData = [
-  {
-    tweetId: '1420166020250042369',
-    userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1641925303/Boop.webp',
-    userName: 'TruBloop',
-    userHandle: 'trubloop',
-    tweetBody:
-      "ok. if you are seeking help but don't want to go from therapist to therapist trying to find the right one... go through @YouAreRAD. I found my current therapist through them and honestly I could not be happier? This therapist actually listens and gets me. We CLICKED. thats HUGE.",
-    tweetTime: '4:35pm',
-    tweetDate: 'Jul 27, 2021',
-    tweetComments: '1',
-    tweetRetweets: '28',
-    tweetLikes: '8',
-  },
+	{
+		tweetId: '1420166020250042369',
+		userProfileImage: 'https://res.cloudinary.com/df23ubjbb/image/upload/v1641925303/Boop.webp',
+		userName: 'TruBloop',
+		userHandle: 'trubloop',
+		tweetBody:
+			"ok. if you are seeking help but don't want to go from therapist to therapist trying to find the right one... go through @YouAreRAD. I found my current therapist through them and honestly I could not be happier? This therapist actually listens and gets me. We CLICKED. thats HUGE.",
+		tweetTime: '4:35pm',
+		tweetDate: 'Jul 27, 2021',
+		tweetComments: '1',
+		tweetRetweets: '8',
+		tweetLikes: '28',
+	},
 ]
