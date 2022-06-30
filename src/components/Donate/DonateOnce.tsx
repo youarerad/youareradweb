5f0803e7f982a6357c7a157544218f88352d43e0
Jason Docton <Jdocton@gmail.com>
2022-06-30 12:39:36 -0700

    Fixed paypal default donation amount

--- src/components/Donate/DonateOnce.tsx
+++ src/components/Donate/DonateOnce.tsx
@@ -64,7 +64,7 @@ export default function DonateOnce() {
 					type="submit"
 					disabled={isLoading}
 				/>
-				<PayPalButton donationValue={30} />
+				<PayPalButton donationValue={selectedOption.priceId} />
 			</form>
 		</>
 	)
