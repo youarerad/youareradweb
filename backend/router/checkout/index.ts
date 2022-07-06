42912fdfa96600fa324a2354298c5ed430d40bfa
KATT <alexander@n1s.se>
2022-07-06 18:57:48 +0200

    add context example

--- backend/router/checkout/index.ts
+++ backend/router/checkout/index.ts
@@ -1,35 +1,34 @@
-import * as trpc from '@trpc/server'
 import { createMonthlyDonation, createOneTimeDonation } from './create-checkout'
 import getSessionById from './getSessionById'
 import { z } from 'zod'
+import { createRouter } from '../createRouter'
 
 /* index.ts, which will return simply as 'checkout', is where we outline the exact type definitions of our checkout router. Each router feature, here including get-session, create-one-time-donation, and create-monthly-donation will expect 'input' followed by the their respective definition of input. */
 
-export const checkoutRouter = trpc
-  .router()
-  .query('get-session', {
-    input: z.object({
-      session_id: z
-        .string()
-        .refine((id) => id.startsWith('cs_'), { message: 'Session ID must start with "cs_"' }),
-    }),
-    async resolve({ input }) {
-      return await getSessionById(input.session_id)
-    },
-  })
-  .mutation('create-onetime-donation', {
-    input: z.object({
-      priceId: z.number(),
-    }),
-    async resolve({ input }) {
-      return await createOneTimeDonation(input.priceId)
-    },
-  })
-  .mutation('create-monthly-donation', {
-    input: z.object({
-      priceId: z.string(),
-    }),
-    async resolve({ input }) {
-      return await createMonthlyDonation(input.priceId)
-    },
-  })
+export const checkoutRouter = createRouter()
+	.query('get-session', {
+		input: z.object({
+			session_id: z
+				.string()
+				.refine((id) => id.startsWith('cs_'), { message: 'Session ID must start with "cs_"' }),
+		}),
+		async resolve({ input }) {
+			return await getSessionById(input.session_id)
+		},
+	})
+	.mutation('create-onetime-donation', {
+		input: z.object({
+			priceId: z.number(),
+		}),
+		async resolve({ input }) {
+			return await createOneTimeDonation(input.priceId)
+		},
+	})
+	.mutation('create-monthly-donation', {
+		input: z.object({
+			priceId: z.string(),
+		}),
+		async resolve({ input }) {
+			return await createMonthlyDonation(input.priceId)
+		},
+	})
--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,11 @@
 import * as trpcNext from '@trpc/server/adapters/next'
 
 import { appRouter } from 'backend/router'
+import { createContext } from 'backend/router/context'
 
 export type AppRouter = typeof appRouter
 
 export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: () => null,
+	router: appRouter,
+	createContext,
 })
