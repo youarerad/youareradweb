3b7c543608c5be6550d5ede115a2551ade5c348a
Guillermo Cava <gcavanunez@gmail.com>
2022-07-24 15:47:43 -0500

    refactor: adding disabled state to radio group, memoize if group is disabled and make graph larger on desktop

--- src/components/Donate/AppRadioGroup.tsx
+++ src/components/Donate/AppRadioGroup.tsx
@@ -11,11 +11,18 @@ interface RadioButtonProps {
 	onChange: (value: number) => void
 	options: RadioOption[]
 	className: string
+	disabled?: boolean
 }
 
-export default function AppRadioGroup({ value, onChange, options, ...rest }: RadioButtonProps) {
+export default function AppRadioGroup({
+	value,
+	onChange,
+	options,
+	disabled,
+	...rest
+}: RadioButtonProps) {
 	return (
-		<RadioGroup value={value} onChange={onChange} {...rest}>
+		<RadioGroup value={value} onChange={onChange} disabled={disabled} {...rest}>
 			{options.map((option) => (
 				<div key={option.value}>
 					<RadioGroup.Label key={option.value} className="sr-only">
@@ -23,11 +30,14 @@ export default function AppRadioGroup({ value, onChange, options, ...rest }: Rad
 					</RadioGroup.Label>
 					<RadioGroup.Option
 						value={option.value}
-						className={({ active, checked }) =>
+						className={({ active, checked, disabled }) =>
 							classNames(
 								active ? 'ring-4 ring-secondary-light ring-opacity-60' : '',
 								checked ? 'bg-secondary border-secondary text-white shadow-xl ' : '',
-								'relative flex items-center justify-center w-full p-3 font-bold text-center transition-all duration-100 border-2 cursor-pointer border-gray-light rounded-xl hover:bg-secondary hover:border-secondary hover:text-white focus:outline-none'
+								disabled
+									? 'opacity-75'
+									: 'opacity-100 hover:bg-secondary hover:border-secondary hover:text-white',
+								'relative flex items-center justify-center w-full p-3 font-bold text-center transition-all duration-100 border-2 cursor-pointer border-gray-light rounded-xl focus:outline-none select-none'
 							)
 						}
 					>
