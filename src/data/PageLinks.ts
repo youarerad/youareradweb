e893a23ed17fb1fb8632ae30da4d1bd44e2a74fb
Guillermo Cava <gcavanunez@gmail.com>
2022-07-24 15:10:21 -0500

    feat: tailwindcss config types and adding link to donate page to navbar

--- src/data/PageLinks.ts
+++ src/data/PageLinks.ts
@@ -1,87 +1,88 @@
 export const corePages = [
-  { name: 'About us', href: '/about' },
-  {
-    name: 'Start therapy',
-    href: '/therapy',
-  },
+	{ name: 'About us', href: '/about' },
+	{
+		name: 'Start therapy',
+		href: '/therapy',
+	},
+	{ name: 'Donate', href: '/donate' },
 ]
 
 export const supportPages = [
-  {
-    name: 'Careers',
-    href: '/careers',
-    icon: '/icons/Careers.svg',
-  },
-  {
-    name: 'Volunteer',
-    href: '/volunteer',
-    icon: '/icons/Volunteer.svg',
-  },
-  {
-    name: 'Partner',
-    href: '/partner',
-    icon: '/icons/Partner.svg',
-  },
+	{
+		name: 'Careers',
+		href: '/careers',
+		icon: '/icons/Careers.svg',
+	},
+	{
+		name: 'Volunteer',
+		href: '/volunteer',
+		icon: '/icons/Volunteer.svg',
+	},
+	{
+		name: 'Partner',
+		href: '/partner',
+		icon: '/icons/Partner.svg',
+	},
 
-  {
-    name: 'Contact',
-    href: '/contact',
-    icon: '/icons/Contact.svg',
-  },
+	{
+		name: 'Contact',
+		href: '/contact',
+		icon: '/icons/Contact.svg',
+	},
 ]
 
 export const fundraisePages = [
-  {
-    name: 'Join the guild',
-    href: '/guild',
-    context: 'Become a member of our legendary community.',
-    image: '/icons/Guild.webp',
-  },
-  {
-    name: 'Live stream fundraiser',
-    href: '/stream',
-    context: 'Empower your community to change the world.',
-    image: '/icons/Stream.webp',
-  },
+	{
+		name: 'Join the guild',
+		href: '/guild',
+		context: 'Become a member of our legendary community.',
+		image: '/icons/Guild.webp',
+	},
+	{
+		name: 'Live stream fundraiser',
+		href: '/stream',
+		context: 'Empower your community to change the world.',
+		image: '/icons/Stream.webp',
+	},
 ]
 
 export const mobileNav = [
-  {
-    name: 'About us',
-    href: '/about',
-  },
-  {
-    name: 'RAD Careers',
-    href: '/careers',
-  },
-  {
-    name: 'Volunteering',
-    href: '/volunteer',
-  },
-  {
-    name: 'Partnerships',
-    href: '/partner',
-  },
+	{
+		name: 'About us',
+		href: '/about',
+	},
+	{
+		name: 'RAD Careers',
+		href: '/careers',
+	},
+	{
+		name: 'Volunteering',
+		href: '/volunteer',
+	},
+	{
+		name: 'Partnerships',
+		href: '/partner',
+	},
 
-  {
-    name: 'Join the guild',
-    href: '/guild',
-  },
-  {
-    name: 'Stream fundraiser',
-    href: '/stream',
-  },
+	{
+		name: 'Join the guild',
+		href: '/guild',
+	},
+	{
+		name: 'Stream fundraiser',
+		href: '/stream',
+	},
 ]
 
 export const footerPages = [
-  { name: 'About', href: '/about' },
-  { name: 'Therapy', href: '/therapy' },
-  { name: 'Donate', href: '/donate' },
-  { name: 'Stream', href: '/stream' },
-  { name: 'Careers', href: '/careers' },
-  { name: 'Volunteer', href: '/volunteer' },
-  { name: 'Partner', href: '/partner' },
-  { name: 'Contact', href: '/contact' },
-  { name: 'Privacy', href: '/privacy' },
-  { name: 'Terms', href: '/terms' },
+	{ name: 'About', href: '/about' },
+	{ name: 'Therapy', href: '/therapy' },
+	{ name: 'Donate', href: '/donate' },
+	{ name: 'Stream', href: '/stream' },
+	{ name: 'Careers', href: '/careers' },
+	{ name: 'Volunteer', href: '/volunteer' },
+	{ name: 'Partner', href: '/partner' },
+	{ name: 'Contact', href: '/contact' },
+	{ name: 'Privacy', href: '/privacy' },
+	{ name: 'Terms', href: '/terms' },
 ]
